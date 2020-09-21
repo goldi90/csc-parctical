bd99cbe14f77a19dd1e813397044791f64e05ff3
goldi90 <kaushikdwivedi22@gmail.com>
2020-09-21 16:39:33 +0530

    deleted unneccesaary files

--- 1.php
+++
@@ -1,4 +0,0 @@
-<?php
-# Installation of XAMPP Server and print “hello world” in PHP.
-echo "Hello world\n";
-?>
--- 2.php
+++
@@ -1,7 +0,0 @@
-<?php
-# Write a PHP program to print sum of digits.
- $k=9;
- $l=7;
- $p=$k+$l;
- echo "the sum of ".$k." and ".$l." is ".$p; 
-?>
